f58c5fca7cbc0f8ab36d7c352311ad5704af4557
Casey <dinnerdoggy@gmail.com>
2025-03-18 21:48:06 -0500

    entities and seed data erected.

---
+++ Migrations/20250319023702_InitialCreate.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TunaBouzouki.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Age = table.Column<int>(type: "integer", nullable: false),
+                    Bio = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    ArtistId = table.Column<int>(type: "integer", nullable: false),
+                    Album = table.Column<string>(type: "text", nullable: false),
+                    Length = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Songs_Artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GenreSong",
+                columns: table => new
+                {
+                    GenresId = table.Column<int>(type: "integer", nullable: false),
+                    SongsId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GenreSong", x => new { x.GenresId, x.SongsId });
+                    table.ForeignKey(
+                        name: "FK_GenreSong_Genres_GenresId",
+                        column: x => x.GenresId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GenreSong_Songs_SongsId",
+                        column: x => x.SongsId,
+                        principalTable: "Songs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Artists",
+                columns: new[] { "Id", "Age", "Bio", "Name" },
+                values: new object[,]
+                {
+                    { 1, 36, "Heavy Deathmetal Band from Buffalo, New York", "Cannibal Corpse" },
+                    { 2, 13, "Technical Deathcore band based on the internet. The band members live in different parts of the world", "Infant Annihilator" },
+                    { 3, 23, "Experimental loop artist band from NYC", "Battles" },
+                    { 4, 27, "Alt-indie psychedelic", "Animal Collective" },
+                    { 5, 70, "English Folk Singer", "Shirley Collins" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Description" },
+                values: new object[,]
+                {
+                    { 1, "Deathmetal" },
+                    { 2, "Deathcore" },
+                    { 3, "Avant-Garde" },
+                    { 4, "Indie" },
+                    { 5, "Folk" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Songs",
+                columns: new[] { "Id", "Album", "ArtistId", "Length", "Title" },
+                values: new object[,]
+                {
+                    { 1, "English Fold Classics", 5, 316, "Barbara Allen" },
+                    { 2, "The Elysian Grandeval Galèriarch", 2, 1741, "Behold the Kingdom of the Wretched Undying" },
+                    { 3, "Juice B Crypts", 3, 523, "A Loop So Nice / They Played it Twice" },
+                    { 4, "Time Skiffs", 4, 629, "Prester John" },
+                    { 5, "Torture", 1, 444, "Scourge of Iron" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GenreSong_SongsId",
+                table: "GenreSong",
+                column: "SongsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_ArtistId",
+                table: "Songs",
+                column: "ArtistId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GenreSong");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Songs");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+        }
+    }
+}
